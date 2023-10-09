09da6af31ba934b9f57802243a23f96f41aa6a9d
eweiberl <eweiberl@student.42heilbronn.de>
2023-10-09 16:33:29 +0200

    first test

--- main.c
+++ main.c
@@ -6,7 +6,18 @@
 /*   By: eweiberl <eweiberl@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 16:20:10 by eweiberl          #+#    #+#             */
-/*   Updated: 2023/10/09 16:20:11 by eweiberl         ###   ########.fr       */
+/*   Updated: 2023/10/09 16:31:25 by eweiberl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minishell.h"
+
+int	main(int argc, char *argv[], char *envp[])
+{
+	char	*test;
+
+	test = readline("Hello there");
+	printf("%s\n", test);
+	free(test);
+	return (0);
+}
