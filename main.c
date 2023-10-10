67fe8f405e5c3188385895f72fc2d965caf3a645
eweiberl <eweiberl@student.42heilbronn.de>
2023-10-10 15:59:07 +0200

    Commented stuff

--- main.c
+++ main.c
@@ -6,13 +6,13 @@
 /*   By: eweiberl <eweiberl@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 16:20:10 by eweiberl          #+#    #+#             */
-/*   Updated: 2023/10/09 18:41:11 by eweiberl         ###   ########.fr       */
+/*   Updated: 2023/10/10 15:26:10 by eweiberl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main(int argc, char *argv[], char *envp[])
+int	main(int argc, char **argv, char **envp)
 {
 	char	*test;
 	char	*prompt;
