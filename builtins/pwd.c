1ff84593989119643aa841d885f57152942f342e
eweiberl <eweiberl@student.42heilbronn.de>
2023-10-11 17:44:40 +0200

    changed the builtin to use getenv

--- builtins/pwd.c
+++ builtins/pwd.c
@@ -6,7 +6,7 @@
 /*   By: eweiberl <eweiberl@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 17:35:08 by eweiberl          #+#    #+#             */
-/*   Updated: 2023/10/11 17:40:40 by eweiberl         ###   ########.fr       */
+/*   Updated: 2023/10/11 17:44:19 by eweiberl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,11 @@
 
 int	builtin_pwd(char **envp)
 {
-	int		i;
 	char	*pwd_str;
 
-	i = 0;
 	pwd_str = NULL;
-	while (envp[i] != NULL)
-	{
-		if (ft_strncmp(envp[i], "PWD=", 4) == 0)
-		{
-			pwd_str = envp[i] + 4;
-			break ;
-		}
-		i++;
-	}
+	(void)envp;
+	pwd_str = getenv("PWD");
 	if (pwd_str == NULL)
 		return (perror("pwd not found"), 1);
 	printf("%s\n", pwd_str);
