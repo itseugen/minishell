43dd933b2407a83067449c266656985e109b4339
eweiberl <eweiberl@student.42heilbronn.de>
2023-10-13 14:02:13 +0200

    pwd uses getcwd now

--- builtins/pwd.c
+++ builtins/pwd.c
@@ -6,7 +6,7 @@
 /*   By: eweiberl <eweiberl@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 17:35:08 by eweiberl          #+#    #+#             */
-/*   Updated: 2023/10/11 17:44:19 by eweiberl         ###   ########.fr       */
+/*   Updated: 2023/10/13 14:01:07 by eweiberl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,15 @@ int	builtin_pwd(char **envp)
 
 	pwd_str = NULL;
 	(void)envp;
-	pwd_str = getenv("PWD");
+	// pwd_str = getenv("PWD");
+	// if (pwd_str == NULL)
+	// 	return (perror("pwd not found"), 1);
+	// printf("%s\n", pwd_str);
+
+	pwd_str = getcwd(NULL, 0);
 	if (pwd_str == NULL)
 		return (perror("pwd not found"), 1);
 	printf("%s\n", pwd_str);
+	free(pwd_str);
 	return (0);
 }
