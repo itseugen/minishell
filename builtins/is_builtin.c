fa2ec33269a8a24eed63811708fbce61902131b3
eweiberl <eweiberl@student.42heilbronn.de>
2023-10-17 16:12:15 +0200

    splited function into bool and executing

--- builtins/is_builtin.c
+++ builtins/is_builtin.c
@@ -6,7 +6,7 @@
 /*   By: eweiberl <eweiberl@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 17:17:58 by eweiberl          #+#    #+#             */
-/*   Updated: 2023/10/16 17:29:46 by eweiberl         ###   ########.fr       */
+/*   Updated: 2023/10/17 16:08:56 by eweiberl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,48 @@
 /// @brief Checks if the str is a builtin function and executes
 /// @param str 
 /// @return false if it isnt, true if it is
-bool	is_builtin(char *str, char *args, char **envp)
+bool	is_builtin(char *str)
 {
 	if (str == NULL)
 		return (false);
 	if (ft_strncmp(str, "cd", 3) == 0)
-		builtin_cd(args);
+		return (true);
 	// else if (ft_strncmp(str, "echo", 5) == 0)
-	// 	builtin_echo(str, args);
+	// 	return (true);
 	else if (ft_strncmp(str, "pwd", 4) == 0)
-		builtin_pwd(envp);
+		return (true);
 	// else if (ft_strncmp(str, "export", 7) == 0)
-	// 	builtin_export(envp);
+	// 	return (true);
 	// else if (ft_strncmp(str, "unset", 6) == 0)
-	// 	builtin_unset(envp);
+	// 	return (true);
 	else if (ft_strncmp(str, "env", 4) == 0)
-		builtin_env(envp);
+		return (true);
 	// else if (ft_strncmp(str, "exit", 5) == 0)
-	// 	builtin_exit(envp);
+	// 	return (true);
 	else
 		return (false);
 	return (true);
+}
+
+int	execute_builtin(char *str, char *args, char **envp)
+{
+	if (str == NULL)
+		return (1);
+	if (ft_strncmp(str, "cd", 3) == 0)
+		return (builtin_cd(args));
+	// else if (ft_strncmp(str, "echo", 5) == 0)
+	// 	return (builtin_echo(str, args));
+	else if (ft_strncmp(str, "pwd", 4) == 0)
+		return (builtin_pwd(envp));
+	// else if (ft_strncmp(str, "export", 7) == 0)
+	// 	return (builtin_export(envp));
+	// else if (ft_strncmp(str, "unset", 6) == 0)
+	// 	return (builtin_unset(envp));
+	else if (ft_strncmp(str, "env", 4) == 0)
+		return (builtin_env(envp));
+	// else if (ft_strncmp(str, "exit", 5) == 0)
+	// 	return (builtin_exit(envp));
+	else
+		return (1);
+	return (0);
 }
