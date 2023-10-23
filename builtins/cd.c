8012a8da1a6763fc4ebfa51690c8bc7e29756c65
eweiberl <eweiberl@student.42heilbronn.de>
2023-10-23 15:03:32 +0200

    added ~

--- builtins/cd.c
+++ builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: eweiberl <eweiberl@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/13 13:46:10 by eweiberl          #+#    #+#             */
-/*   Updated: 2023/10/13 15:27:32 by eweiberl         ###   ########.fr       */
+/*   Updated: 2023/10/23 15:03:24 by eweiberl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	builtin_cd(char *path)
 	int		ret;
 	char	*home_dir;
 
-	if (path == NULL || path[0] == '\0')
+	if (path == NULL || path[0] == '\0' || ft_strncmp(path, "~", 2) == 0)
 	{
 		home_dir = getenv("HOME");
 		if (home_dir == NULL)
