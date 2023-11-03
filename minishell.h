3d77d978760c2ddca1e111e76babed11d1b57adc
eweiberl <eweiberl@student.42heilbronn.de>
2023-11-03 17:07:42 +0100

    included new header

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: eweiberl <eweiberl@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 16:20:13 by eweiberl          #+#    #+#             */
-/*   Updated: 2023/10/24 17:17:19 by eweiberl         ###   ########.fr       */
+/*   Updated: 2023/11/03 17:07:34 by eweiberl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@
 # include <stdio.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+# include <fcntl.h>
 
 /* ************************************************************************** */
 /*                            Defines & enums                                 */
