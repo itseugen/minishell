80da2b04150037cc496e0f0f5f4ed8d02bd5e5be
eweiberl <eweiberl@student.42heilbronn.de>
2023-11-07 20:32:46 +0100

    Reformatted

--- parser/parsing_doc.c
+++ parser/parsing_doc.c
@@ -1,24 +1,35 @@
-A Recursive Descent Parser is a type of top-down parser used in computer science and programming to analyze the syntax of a given input, typically a sequence of tokens, such as a programming language source code. This parsing technique is called "recursive descent" because it involves recursive function calls to process the input, with each function handling a specific grammar rule or production.
+A Recursive Descent Parser is a type of top-down parser used in computer science and programming to analyze the syntax
+of a given input, typically a sequence of tokens, such as a programming language source code. This parsing technique is called 
+"recursive descent" because it involves recursive function calls to process the input, with each function handling a specific grammar rule or production.
 
 Key characteristics of a recursive descent parser:
 
-Top-Down Parsing: Recursive descent parsers start from the highest-level grammar rule (usually the start symbol) and work their way down to the terminal symbols (tokens) by applying grammar rules.
+Top-Down Parsing: Recursive descent parsers start from the highest-level grammar rule (usually the start symbol) 
+and work their way down to the terminal symbols (tokens) by applying grammar rules.
 
-Predictive Parsing: These parsers are also known as "predictive parsers" because they predict which production to use based on the current input. They typically use the next token or a lookahead token to make this prediction.
+Predictive Parsing: These parsers are also known as "predictive parsers" because they 
+predict which production to use based on the current input. They typically use the next token or a lookahead token to make this prediction.
 
-Separate Functions: Each non-terminal symbol in the grammar corresponds to a separate function in the parser. These functions are recursively called to handle different parts of the input.
+Separate Functions: Each non-terminal symbol in the grammar corresponds to a separate function in the parser. 
+These functions are recursively called to handle different parts of the input.
 
-Terminal and Non-Terminal Symbols: Terminal symbols are the actual tokens from the input, while non-terminal symbols are higher-level syntactic constructs in the grammar.
+Terminal and Non-Terminal Symbols: Terminal symbols are the actual tokens from the input,
+ while non-terminal symbols are higher-level syntactic constructs in the grammar.
 
-Error Handling: Recursive descent parsers can be designed to handle syntax errors gracefully, providing clear and meaningful error messages.
+Error Handling: Recursive descent parsers can be designed to handle syntax errors gracefully, 
+providing clear and meaningful error messages.
 
-Advantages: Recursive descent parsers are relatively easy to implement and understand, especially for LL(1) grammars (grammars that can be parsed with one symbol of lookahead).
+Advantages: Recursive descent parsers are relatively easy to implement and understand, 
+especially for LL(1) grammars (grammars that can be parsed with one symbol of lookahead).
 
 However, there are limitations to recursive descent parsing:
 
 It may not handle left-recursive grammars without modification.
 Handling left-factoring in grammars can be more complex.
 It may not be suitable for some grammars with ambiguity or conflicts.
-To create a recursive descent parser, you typically define a set of parsing functions, each corresponding to a non-terminal symbol in the grammar. These functions are responsible for recognizing the structure defined by the grammar rules and may call other parsing functions as needed.
+To create a recursive descent parser, you typically define a set of parsing functions, each corresponding to a non-terminal 
+symbol in the grammar. These functions are responsible for recognizing the structure defined by the grammar rules and may call other parsing functions as needed.
 
-Overall, recursive descent parsing is a fundamental and widely used technique for building parsers in compilers and interpreters for programming languages. It's especially useful for simpler, LL(1) grammars, where it can provide a straightforward and efficient way to analyze and understand the structure of code.
+Overall, recursive descent parsing is a fundamental and widely used technique for building parsers in compilers and interpreters for
+ programming languages. It's especially useful for simpler, LL(1) grammars, 
+ where it can provide a straightforward and efficient way to analyze and understand the structure of code.
