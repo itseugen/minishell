a63caffe1df2e5c457fef3a0e49e3f62159a1483
Eugen <118663096+itseugen@users.noreply.github.com>
2023-11-09 16:24:20 +0000

    added a function to expand the whole list

---
+++ expander/expand_tokens.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   expand_tokens.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eweiberl <eweiberl@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/09 16:02:45 by eweiberl          #+#    #+#             */
+/*   Updated: 2023/11/09 16:24:07 by eweiberl         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+/// @brief Takes the token list and expands all cmds
+/// @param envp 
+/// @param tokens the token list
+/// @return 1 if the expander fails (malloc) otherwise 0
+int	expand_tokens(char **envp, t_token *tokens)
+{
+	t_token	*current;
+
+	current = tokens;
+	while (current != NULL)
+	{
+		current->cmd = expander(current->cmd, envp);
+		if (current->cmd == NULL)
+			return (1);
+		current = current->next;
+	}
+	return (0);
+}
