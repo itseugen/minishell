7af3b81a68f5b9d5256b530273add9e21062ac91
Eugen <118663096+itseugen@users.noreply.github.com>
2023-11-09 17:16:12 +0000

    moved function to top for readability

--- lexer/check_token.c
+++ lexer/check_token.c
@@ -6,11 +6,36 @@
 /*   By: eweiberl <eweiberl@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 16:43:15 by eweiberl          #+#    #+#             */
-/*   Updated: 2023/11/09 17:09:45 by eweiberl         ###   ########.fr       */
+/*   Updated: 2023/11/09 17:14:33 by eweiberl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+/// @brief scans the lexer and assigns IDs
+/// @param token_list 
+void	assign_id(t_token *token_list)
+{
+	t_token	*current;
+
+	current = token_list;
+	while (current != NULL)
+	{
+		if (current->cmd[0] == '|')
+			current->operation = PIPE;
+		else if (current->cmd[0] == '<' && current->cmd[1] == '<')
+			current->operation = HERE_DOC;
+		else if (current->cmd[0] == '<' || current->cmd[0] == '>')
+			current->operation = REDIRECT;
+		else if (current->cmd[0] == ';')
+			current->operation = SEMICOLON;
+		else if (is_builtin(current->cmd) == true)
+			current->operation = BUILTIN;
+		else
+			current->operation = CMD;
+		current = current->next;
+	}
+}
 
 bool	is_redirect(char *cmd)
 {
@@ -43,29 +68,4 @@ bool	is_builtin(char *cmd)
 	if (ft_strncmp(cmd, "exit ", 5) == 0)
 		return (true);
 	return (false);
-}
-
-/// @brief scans the lexer and assigns IDs
-/// @param token_list 
-void	assign_id(t_token *token_list)
-{
-	t_token	*current;
-
-	current = token_list;
-	while (current != NULL)
-	{
-		if (current->cmd[0] == '|')
-			current->operation = PIPE;
-		else if (current->cmd[0] == '<' && current->cmd[1] == '<')
-			current->operation = HERE_DOC;
-		else if (current->cmd[0] == '<' || current->cmd[0] == '>')
-			current->operation = REDIRECT;
-		else if (current->cmd[0] == ';')
-			current->operation = SEMICOLON;
-		else if (is_builtin(current->cmd) == true)
-			current->operation = BUILTIN;
-		else
-			current->operation = CMD;
-		current = current->next;
-	}
 }
