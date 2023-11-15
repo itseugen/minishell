b316f594af9798f3f68187286bc27fb70de87e10
Eugen <118663096+itseugen@users.noreply.github.com>
2023-11-15 15:47:22 +0000

    Changed form perror to fprintf

--- builtins/pwd.c
+++ builtins/pwd.c
@@ -6,7 +6,7 @@
 /*   By: eweiberl <eweiberl@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 17:35:08 by eweiberl          #+#    #+#             */
-/*   Updated: 2023/11/15 15:09:08 by eweiberl         ###   ########.fr       */
+/*   Updated: 2023/11/15 15:47:08 by eweiberl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	builtin_pwd(t_env *env_list)
 
 	pwd_str = get_env_var("PWD", env_list);
 	if (pwd_str == NULL)
-		return (perror("pwd not found"), 1);
+		return (ft_fprintf(2, "pwd not found\n"), 1);
 	printf("%s\n", pwd_str);
 	return (0);
 }
