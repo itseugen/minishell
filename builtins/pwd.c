b1d7618bae98dc5085e04ae2307220767abcd272
Eugen <118663096+itseugen@users.noreply.github.com>
2023-11-15 15:29:46 +0000

    Revisited pwd

--- builtins/pwd.c
+++ builtins/pwd.c
@@ -6,27 +6,19 @@
 /*   By: eweiberl <eweiberl@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 17:35:08 by eweiberl          #+#    #+#             */
-/*   Updated: 2023/10/13 14:01:07 by eweiberl         ###   ########.fr       */
+/*   Updated: 2023/11/15 15:09:08 by eweiberl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-int	builtin_pwd(char **envp)
+int	builtin_pwd(t_env *env_list)
 {
 	char	*pwd_str;
 
-	pwd_str = NULL;
-	(void)envp;
-	// pwd_str = getenv("PWD");
-	// if (pwd_str == NULL)
-	// 	return (perror("pwd not found"), 1);
-	// printf("%s\n", pwd_str);
-
-	pwd_str = getcwd(NULL, 0);
+	pwd_str = get_env_var("PWD", env_list);
 	if (pwd_str == NULL)
 		return (perror("pwd not found"), 1);
 	printf("%s\n", pwd_str);
-	free(pwd_str);
 	return (0);
 }
