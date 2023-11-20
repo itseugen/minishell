31596b538922d52a51af732b959eb388087b9c50
Eugen <118663096+itseugen@users.noreply.github.com>
2023-11-20 13:57:26 +0000

    Works without PWD

--- builtins/pwd.c
+++ builtins/pwd.c
@@ -6,13 +6,11 @@
 /*   By: eweiberl <eweiberl@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 17:35:08 by eweiberl          #+#    #+#             */
-/*   Updated: 2023/11/20 13:18:42 by eweiberl         ###   ########.fr       */
+/*   Updated: 2023/11/20 13:53:01 by eweiberl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
-
-//! CHANGE USING GETCWD, PWD HAS TO WORK WITH PWD UNSER
 
 /// @brief Prints the current working directory
 /// @param env_list the environment list
@@ -21,9 +19,11 @@ int	builtin_pwd(t_env *env_list)
 {
 	char	*pwd_str;
 
-	pwd_str = get_env_var("PWD", env_list);
+	(void)env_list;
+	pwd_str = getcwd(NULL, 0);
 	if (pwd_str == NULL)
-		return (ft_fprintf(2, "pwd not found\n"), 1);
+		return (ft_fprintf(2, "pwd: getcwd fail\n"), 1);
 	printf("%s\n", pwd_str);
+	free(pwd_str);
 	return (0);
 }
