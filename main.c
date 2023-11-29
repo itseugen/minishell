d5bfac19aa25c259723a2fcc91be8a9ff219c810
eweiberl <eweiberl@student.42heilbronn.de>
2023-11-29 18:04:51 +0100

    splitted main and created get_and tokenize function

--- main.c
+++ main.c
@@ -6,81 +6,29 @@
 /*   By: eweiberl <eweiberl@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 16:20:10 by eweiberl          #+#    #+#             */
-/*   Updated: 2023/11/29 17:29:13 by eweiberl         ###   ########.fr       */
+/*   Updated: 2023/11/29 18:03:54 by eweiberl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-/*
-* TODO:
-* - Parser - done
-* - executing
-* - missing builtins
-* - Syntax tree / command table
-* - Signals
-* - ctrl inputs
-*/
-
-/*
-!Idea: Pass a pointer to the string returned by readline instead of using split
-**
-** char **cmd_table[]
-** each index represents a command, the space between means to pipe
-** we use minishell split to split the command, we can then
-** check for </>/>> to pipe to file
-**
-*/
-
-/*
-** For the expander:
-** IDEA: It take a string, checks for expandable variables
-** if it finds any it expands them,
-** frees the old string and returns a new one with the expanded variable
-** else it just returns the old string
-*/
-
-// void	leaks()
-// {
-// 	system("leaks minishell");
-// }
+static int		setup(int argc, char **argv, t_env *env_list);
+static t_token	*get_and_tokenize(t_env *env_list);
 
 int	main(int argc, char **argv, char **envp)
 {
-	char	*line;
 	t_token	*tokens;
 	t_env	*env_list;
 	t_exec	**exec;
 
-	signal(SIGINT, sig_type);
-	signal(SIGQUIT, sig_type);
-	clear_sigargs();
-	(void)argc;
-	(void)argv;
-	exec = NULL;
 	env_list = env_init(envp);
+	if (setup(argc, argv, env_list) == 1)
+		exit(1);
 	while (1)
 	{
-		line = readline("minish % ");
-		if (line == NULL) //Controll + D;
-			break ;
-		if (line[0] == '\0')
-			continue ;
-		add_history(line);
-		tokens = tokenizer(line);
-		free(line);
+		tokens = get_and_tokenize(env_list);
 		if (tokens == NULL)
-		{
-			ft_fprintf(2, "Error in tokenizer\n");
 			continue ;
-		}
-		if (tokens->cmd[0] == '\0')
-		{
-			free_tokens(&tokens);
-			continue ;
-		}
-		fix_tokens(tokens);
-		expand_tokens(env_list, tokens);
 		if (mainpars(tokens) == -1)
 		{
 			ft_fprintf(2, "parse error\n");
@@ -96,3 +44,99 @@ int	main(int argc, char **argv, char **envp)
 	free_env_struct(&env_list);
 	return (0);
 }
+
+static int	setup(int argc, char **argv, t_env *env_list)
+{
+	signal(SIGINT, sig_type);
+	signal(SIGQUIT, sig_type);
+	clear_sigargs();
+	(void)argc;
+	(void)argv;
+	if (env_list == NULL)
+	{
+		ft_fprintf(2, "setup: setting up env failed\n", 1);
+		return (1);
+	}
+	return (0);
+}
+
+static t_token	*get_and_tokenize(t_env *env_list)
+{
+	char	*line;
+	t_token	*tokens;
+	char	*prompt;
+
+	prompt = get_prompt();
+	if (prompt == NULL)
+		return (ft_fprintf(2, "prompt: malloc fail\n"), NULL);
+	line = readline(prompt);
+	if (line == NULL)
+	{
+		free_env_struct(&env_list);
+		exit(0);
+	}
+	if (line[0] == '\0')
+		return (NULL);
+	add_history(line);
+	tokens = tokenizer(line);
+	if (tokens == NULL)
+		return (free(line), ft_fprintf(2, "Error in tokenizer\n"), NULL);
+	if (tokens->cmd[0] == '\0')
+		return (free(line), free_tokens(&tokens), NULL);
+	fix_tokens(tokens);
+	if (expand_tokens(env_list, tokens) == 1)
+		return (free(line), free_tokens(&tokens), NULL);
+	return (tokens);
+}
+
+// int	main(int argc, char **argv, char **envp)
+// {
+// 	char	*line;
+// 	t_token	*tokens;
+// 	t_env	*env_list;
+// 	t_exec	**exec;
+
+// 	signal(SIGINT, sig_type);
+// 	signal(SIGQUIT, sig_type);
+// 	clear_sigargs();
+// 	(void)argc;
+// 	(void)argv;
+// 	exec = NULL;
+// 	env_list = env_init(envp);
+// 	while (1)
+// 	{
+// 		line = readline("minish % ");
+// 		if (line == NULL) //Controll + D;
+// 			break ;
+// 		if (line[0] == '\0')
+// 			continue ;
+// 		add_history(line);
+// 		tokens = tokenizer(line);
+// 		free(line);
+// 		if (tokens == NULL)
+// 		{
+// 			ft_fprintf(2, "Error in tokenizer\n");
+// 			continue ;
+// 		}
+// 		if (tokens->cmd[0] == '\0')
+// 		{
+// 			free_tokens(&tokens);
+// 			continue ;
+// 		}
+// 		fix_tokens(tokens);
+// 		expand_tokens(env_list, tokens);
+// 		if (mainpars(tokens) == -1)
+// 		{
+// 			ft_fprintf(2, "parse error\n");
+// 			continue ;
+// 		}
+// 		exec = commands_for_exec(tokens);
+// 		free_tokens(&tokens);
+// 		if (!exec)
+// 			return (ft_fprintf(2, "command initialization error\n"), -1);
+// 		executor(exec, env_list);
+// 	}
+// 	clear_history();
+// 	free_env_struct(&env_list);
+// 	return (0);
+// }
