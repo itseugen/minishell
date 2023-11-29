085483fa2ef29c9aaf5f8c37e9d6038c0807a577
eweiberl <eweiberl@student.42heilbronn.de>
2023-11-29 17:29:19 +0100

    norm fixes

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: eweiberl <eweiberl@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 16:20:10 by eweiberl          #+#    #+#             */
-/*   Updated: 2023/11/29 16:37:58 by eweiberl         ###   ########.fr       */
+/*   Updated: 2023/11/29 17:29:13 by eweiberl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -96,55 +96,3 @@ int	main(int argc, char **argv, char **envp)
 	free_env_struct(&env_list);
 	return (0);
 }
-
-/*
-Display a prompt when waiting for a new command.
-• Have a working history.
-• Search and launch the right executable (based on the PATH variable or using a
-relative or an absolute path).
-• Avoid using more than one global variable to indicate a received signal.
-Consider the implications: this approach ensures that your signal handler
-will not access your main data structures.1
-
-! Not interpret unclosed quotes or special characters which are not required
-!by the subject such as \ (backslash) or ; (semicolon).
-* DONE • Handle ’ (single quote) which should prevent the shell from
-* interpreting the meta-characters in the quoted sequence. interpreting
-* DONE • Handle " (double quote) which should prevent the shell from
-* the meta-characters in the quoted sequence except for $ (dollar sign).
-
-! • Implement redirections:
-! ◦ < should redirect input.
-! ◦ > should redirect output.
-! ◦ << should be given a delimiter, then read the input until a line containing
-! the delimiter is seen. However, it doesn’t have to update the history!
-! ◦ >> should redirect output in append mode.
-
-! • Implement pipes (| character). The output of each command in the pipeline is
-! connected to the input of the next command via a pipe.
-
-* DONE • Handle environment variables ($ followed by a sequence of characters) which
-* should expand to their values.
-
-TODO: Change expander to handle it, use signal/global/getter function
-! • Handle $? which should expand to the exit status of the most recently
-! executed foreground pipeline.
-
-TODO: Part of signal handling
-! • Handle ctrl-C, ctrl-D and ctrl-\ which should behave like in bash.
-• In interactive mode:
-! ◦ ctrl-C displays a new prompt on a new line.
-! ◦ ctrl-D exits the shell.
-! ◦ ctrl-\ does nothing.
-
-• Your shell must implement the following builtins:
-* DONE ◦ echo with option -n
-* DONE ◦ cd with only a relative or absolute path (Doesnt set PATH-Var)
-* DONE ◦ pwd with no options
-* DONE ◦ export with no options
-* DONE ◦ unset with no options
-* DONE ◦ env with no options or arguments
-TODO: Same as ctrl-D -> handle together
-! ◦ exit with no options
-
-*/
