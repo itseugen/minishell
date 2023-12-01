c2c2a7d65e27846287af837f45c671347c11de12
Arvind Dhaka <adhaka@3-E-6.42heilbronn.de>
2023-12-01 06:55:56 +0100

    cleanup

--- parser/parser.c
+++ parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: adhaka <adhaka@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 04:13:05 by adhaka            #+#    #+#             */
-/*   Updated: 2023/12/01 06:52:21 by adhaka           ###   ########.fr       */
+/*   Updated: 2023/12/01 06:54:45 by adhaka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -164,64 +164,3 @@ int	red_maker(t_token *tokens, int flag)
 	return (0);
 }
 
-/// @brief Extracts the input or output file name from the redirection token.
-/// @param str
-/// @param tmp
-/// @param flag
-/// @return 0
-int	in_out(char *str, t_token *tmp, int flag)
-{
-	int		i;
-	int		j;
-	char	*l;
-
-	i = 1;
-	j = 0;
-	if (str[i] == '>' || str[i] == '<')
-		i = 2;
-	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
-		i++;
-	l = (char *)malloc(sizeof(char) * (word_len(str, i) + 1));
-	if (!l)
-		return (-1);
-	while (str[i] && !((str[i] >= 9 && str[i] <= 13) || str[i] == 32))
-	{
-		l[j] = str[i];
-		i++;
-		j++;
-	}
-	l[j] = '\0';
-	if (!flag)
-		tmp->table->input_file = l;
-	else
-		tmp->table->output_file = l;
-	return (0);
-}
-
-/// @brief Opens a file with specific flags based on the provided parameters.
-/// @param str
-/// @param flag
-/// @return fd
-int	ft_open(char *str, int flag)
-{
-	int	fd;
-
-	if (flag == 0)
-	{
-		if (access(str, F_OK) == 0 && access(str, R_OK) == 0)
-		{
-			fd = open(str, O_RDONLY);
-			if (fd < 0)
-				return (ft_fprintf(2, "Error opening input file\n"), -1);
-		}
-		else
-			return (ft_fprintf(2, "Wrong input file\n"), -1);
-	}
-	if (flag == 1)
-		fd = open(str, O_CREAT | O_WRONLY | O_TRUNC, 0644);
-	if (flag == 2)
-		fd = open(str, O_APPEND | O_CREAT | O_WRONLY, 0644);
-	if (fd < 0)
-		return (ft_fprintf(2, "Error opening output file\n"), -1);
-	return (fd);
-}
